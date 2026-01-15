4948db0400d27148ebc82ab04a292fac6bf203b8
Igor <igor.warlog@gmail.com>
2026-01-14 21:53:26 -0300

    feat: create a callback database lawyer, layout modal and create a buttons

--- components/modal_advogados.py
+++ components/modal_advogados.py
@@ -11,10 +11,39 @@ from app import app
 from components import home
 
 # ======== Layout ========= #
-layout = dbc.Modal([])
+layout = dbc.Modal([
+    dbc.ModalHeader(dbc.ModalTitle('Cadastar Novo Advogado')),
+    dbc.ModalBody([
+        dbc.Row([
+            dbc.Col([
+                html.Div(id='table_lawyer', className='dbc')
+            ])
+        ])
+    ]),
+    dbc.ModalFooter([
+        dbc.Button('Sair', id='quit_button', color='danger'),
+        dbc.Button('Novo', id='lawyer-new', color='secundary')
+    ])
+    
+], id='modal_lawyer', size='lg', is_open=False)
             
 
 
 # ====== Callbacks ======= #
 # Tabela com os advogados da empresa
-
+@app.callback(
+Output('table_lawyer', 'children'),
+Input('store_lawyer', 'data')
+)
+def table(data):
+   
+    df = pd.DataFrame(data)
+    df = df.fillna('-')
+    
+    return dash_table.DataTable(
+                id='datatable-lawyers',
+                columns=[{"name": col, "id": col} for col in df.columns],
+                data=df.to_dict('records'),
+                page_size=10,
+                page_current=0
+            )
